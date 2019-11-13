12c4cf439a3e718f36c70f7cadacf545dcbf2177
Ubuntu <ubuntu@ip-172-31-87-23.ec2.internal>
2019-11-13 03:07:41 +0000

    varible changes to calc.rb

--- first_programs/calc.rb
+++ first_programs/calc.rb
@@ -4,8 +4,8 @@ hour = minute * 60
 day = hour * 24
 year = day * 365
 puts  'Hello Friend! How old are you?'
-age = gets.chomp
-varible = age.to_i * year
+age = gets.chomp.to_i
+varible = age * year
 puts 'So that would make you ' + varible.to_s + ' seconds old!'
-days = (age.to_i*year)/day
+days = (age * year)/day
 puts 'Or ' + days.to_s + ' days old'
